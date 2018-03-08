f16e869b0f74e9153c5af93fcecb641c8eb3eae4
Darío Suárez Gracia <dario@unizar.es>
2018-03-08 23:35:00 +0100

    Solved typo in order of variables

--- test_practica2_5.py
+++ test_practica2_5.py
@@ -24,20 +24,20 @@ class TestPractica2_5(unittest.TestCase):
 
             pattern=re.compile('#!/usr/bin/env\s+bash')
             # two options: #!/bin/bash or #!/usr/bin/env bash
-            self.assertTrue((first_line == '#!/bin/bash') or 
+            self.assertTrue((first_line == '#!/bin/bash') or
                     (pattern.match(first_line) != None))
 
     def test_no_dir(self):
         invalid_dir_name=''.join([ random.choice(string.ascii_letters+string.digits+' ') for _ in range(128) ])
-        try: 
+        try:
             self.child = pexpect.spawn('/bin/bash ./practica2_5.sh')
-            self.child.expect('Introduzca el nombre de un directorio: ')
+            self.assertFalse(self.child.expect_exact('Introduzca el nombre de un directorio: '))
             self.child.sendline(invalid_dir_name)
-            self.child.expect('{} no es un directorio'.format(invalid_dir_name))
+            self.assertFalse(self.child.expect_exact('{} no es un directorio'.format(invalid_dir_name)))
         except:
             self.assertTrue(False)
         self.assertTrue(True)
-        
+
         self.child.terminate(force=True)
 
     def test_empty_dir(self):
@@ -45,9 +45,13 @@ class TestPractica2_5(unittest.TestCase):
 
         try:
             self.child = pexpect.spawn('/bin/bash ./practica2_5.sh')
-            self.child.expect('Introduzca el nombre de un directorio: ')
+            self.assertFalse(self.child.expect_exact('Introduzca el nombre de un directorio: '))
+        except:
+            self.assertTrue(False)
+
+        try:
             self.child.sendline(tmp_dir_name)
-            self.child.expect('El numero de ficheros y directorios en {} es de 0 y 0, respectivamente'.format(tmp_dir_name))
+            self.assertFalse(self.child.expect('El numero de ficheros y directorios en {} es de 0 y 0, respectivamente'.format(tmp_dir_name)))
         except:
             self.assertTrue(False)
         self.assertTrue(True)
@@ -57,29 +61,36 @@ class TestPractica2_5(unittest.TestCase):
 
 
     def test_regular_case(self):
-        # create a random number of directories 
+        # create a random number of directories
         tmp_dir_name = mkdtemp(prefix=' with spaces ')
 
         n_dirs=random.randint(1, 16)
         n_files=random.randint(1, 256)
 
         list_dirs= [ mkdtemp(dir=tmp_dir_name) for _ in range(n_dirs) ] + [ tmp_dir_name ]
-
         for _ in range(n_files):
             mkstemp(dir=random.choice(list_dirs), prefix=' louis pouzin')
 
         try:
             self.child = pexpect.spawn('/bin/bash ./practica2_5.sh')
-            self.child.expect('Introduzca el nombre de un directorio: ')
+            self.assertFalse(self.child.expect_exact('Introduzca el nombre de un directorio: '))
             self.child.sendline(tmp_dir_name)
-            self.child.expect('El numero de ficheros y directorios en {} es de {} y {}, respectivamente'.format(tmp_dir_name, n_dirs, n_files))
         except:
+            print self.child.before
+            print str(self.child)
+            self.assertTrue(False, msg="Error sending directory name")
+
+        try:
+            self.assertFalse(self.child.expect_exact('El numero de ficheros y directorios en {} es de {} y {}, respectivamente'.format(tmp_dir_name, n_files, n_dirs)))
+        except:
+            print self.child.before
+            print str(self.child)
             self.assertTrue(False)
-        self.assertTrue(True)
+
+        self.assertFalse(self.child.expect_exact(pexpect.EOF))
 
         rmtree(tmp_dir_name)
         self.child.terminate(force=True)
-    
 
 if __name__ == "__main__":
     unittest.main()
