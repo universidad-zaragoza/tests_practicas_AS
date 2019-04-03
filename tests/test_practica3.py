f975ff539ea0c508353e2dfe451cc896b6e2b2bc
Darío Suárez Gracia <dario@unizar.es>
2019-04-03 22:57:46 +0200

    Updated UID for 2018/2019

--- tests/test_practica3.py
+++ tests/test_practica3.py
@@ -49,7 +49,7 @@ class TestPractica3(unittest.TestCase):
                 if they exists:
                 -U
                 -k /etc/skel
-                -K UID_MIN=1000
+                -K UID_MIN=1815
                 -c
              """
 
@@ -78,7 +78,7 @@ class TestPractica3(unittest.TestCase):
             return required_options
 
         required_commands=set(['useradd', 'userdel', 'usermod', 'chpasswd', 'tar'])
-        required_useradd_options=set(['-U',  '-k /etc/skel', '-K UID_MIN=1000', '-c'])
+        required_useradd_options=set(['-U',  '-k /etc/skel', '-K UID_MIN=1815', '-c'])
 
         with open(self.script_name) as f:
             # flag for checking that at least one invocation to useradd includes all required options
